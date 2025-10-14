064716d183d6bbea068fd3aa6462da59ae857023
NorwilJan <norwilaxie@gmail.com>
2025-10-14 10:54:40 +0800

    Update home.js

--- my-website/js/home.js
+++ my-website/js/home.js
@@ -282,7 +282,7 @@ function displayShopeeLinks() {
             // YOUR SHOPEE LINK
             url: 'https://collshp.com/reelroom', 
             // Placeholder image - Replace with a hosted image URL for your product/store banner
-            img: 'https://via.placeholder.com/150x225?text=Visit+Shopee+Store', 
+            img: 'https://down-ph.img.susercontent.com/file/ph-11134207-7ras9-m8on3bi10kula8@resize_w1080_nl.webp', 
             alt: 'ReelRoom Official Shopee Store'
         },
         { 
